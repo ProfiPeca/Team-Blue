f384151728beee435b501ecfb29b1d93611d3ae2
Domysli-si <s.maj683@gmail.com>
2025-12-18 13:06:00 +0100

    uprava

--- blue-team/server.js
+++ blue-team/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
 const fs = require('fs');
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
 
 const PORT = 3002;
 const STORAGE_FILE = 'storage.json';
